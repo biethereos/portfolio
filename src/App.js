66e5ddd4af19c67f02ab7ab9be908bf44e1102f6
NhaT Tien <nguyenthanhhung@gmail.com>
2022-04-02 12:21:05 +0700

    pages and folder structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Courses from './pages/Courses';
+import Contact from './pages/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Portfolio</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path='/projects' element={<Projects />} />
+					<Route path='/courses' element={<Courses />} />
+					<Route path='/contact' element={<Contact />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
