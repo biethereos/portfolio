082200ddb8f439636b32693171638dc318239e2b
biethereos <nguyenthanhhung@gmail.com>
2022-09-01 16:26:04 +0700

    Intro Section Skeleton

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Courses from './pages/Courses';
-import Contact from './pages/Contact';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Courses from "./pages/Courses";
+import Contact from "./pages/Contact";
+import "./stylesheets/home.css";
 
 function App() {
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path='/projects' element={<Projects />} />
-					<Route path='/courses' element={<Courses />} />
-					<Route path='/contact' element={<Contact />} />
-				</Routes>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
     <div>
-        <h1>Home</h1>
+      <div
+        className="introduction flex-with-center"
+        style={{ backgroundImage: `url('./assets/images/bg_portfolio.svg')` }}
+      >
+        <div>
+          <h1>HungNguyen</h1>
+          <div className="intro-content d-flex justify-content-between">
+            <p>
+              MERN Stack <br /> Developer, Python
+            </p>
+            <button className="primary-button">Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
