aac74a293a09076d16dd0bce238e185b7fcb72a3
biethereos <nguyenthanhhung@gmail.com>
2022-10-01 22:53:02 +0700

    HeaderStyling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 function Header() {
   const [showHeader, setShowHeader] = useState(false);
@@ -16,6 +17,21 @@ function Header() {
           className="menu-icon position-fixed top-0 end-0"
         />
       )}
+
+      <ul className={`${showHeader ? "show-header" : "hide-header"} n-box1`}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+        <li>
+          <Link to="/courses">Courses</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </div>
   );
 }
